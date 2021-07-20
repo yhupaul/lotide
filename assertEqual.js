72f80c5148dace5755ced58ac997122dd819e939
yhupaul <yhupaul@hotmail.com>
2021-07-19 19:54:54 -0700

    template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("🙅‍♂️🙅‍♂️🙅‍♂️" + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log("🙆‍♂️🙆‍♂️🙆‍♂️" + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
