be6103f08bbad029be7248abd1f17e4747cd8a25
yhupaul <yhupaul@hotmail.com>
2021-07-19 19:43:07 -0700

    console.assert

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🙅‍♂️🙅‍♂️🙅‍♂️" + " Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log("🙆‍♂️🙆‍♂️🙆‍♂️" + " Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
