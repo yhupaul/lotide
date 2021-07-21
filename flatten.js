a76b56ea7322867402871e58e84951bf25184459
yhupaul <yhupaul@hotmail.com>
2021-07-21 00:13:47 -0700

    I use .flast() method

---
+++ flatten.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  if (!(eqArrays(arrayOne, arrayTwo))) {
+    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed`);
+  } else {
+    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed`);
+  }
+};
+
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const flatten = function (array) {
+  return array.flat()
+}
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
