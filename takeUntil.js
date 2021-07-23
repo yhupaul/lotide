a4895af5c7e89fe8e73853e037efef5f3470a0b1
yhupaul <yhupaul@hotmail.com>
2021-07-22 18:57:42 -0700

    new function with callback

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (!(eqArrays(arrayOne, arrayTwo))) {
+    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed`);
+  } else {
+    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed`);
+  }
+};
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const takeUntil = function(array, callback) {
+  let newArr = [];
+  for (let element of array) {
+    if (callback(element)) {
+      return newArr
+    } else {
+      newArr.push(element)
+    }
+  }
+}
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
