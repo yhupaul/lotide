6cae2741f3e6efa58e892030cfa3c76f0a0f8ab3
yhupaul <yhupaul@hotmail.com>
2021-07-26 14:37:33 -0700

    refator for unit testing

--- tail.js
+++ tail.js
@@ -1,20 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
 
 let tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail(words);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(result.length, 2);
+module.exports = tail;
