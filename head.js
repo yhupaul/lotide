80620432fbd938cb38205d0efb8dfc7d061ee857
yhupaul <yhupaul@hotmail.com>
2021-07-26 12:55:22 -0700

    refactor for uni testing

--- head.js
+++ head.js
@@ -1,8 +1,6 @@
-const assertEqual = require('./assertEqual');
 
 let head = function(array) {
   return array[0];
 };
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
