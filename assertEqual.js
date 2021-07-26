2f66a087892c6d8a9be414f83fffeea27fcd6b79
yhupaul <yhupaul@hotmail.com>
2021-07-26 12:49:49 -0700

    refactor uti test

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,11 @@
 const assertEqual = function(actual, expected) {
+  
   if (actual !== expected) {
     console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
   } else {
     console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
   }
+  
 };
-
-
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
 
 module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
