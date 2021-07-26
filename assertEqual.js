3883d83ae2f596f308a3fc7576ee6aa3055a1f1a
yhupaul <yhupaul@hotmail.com>
2021-07-26 07:31:39 -0700

    refactor for uni test

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+module.exports = assertEqual;
