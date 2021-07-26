b7fe2a7a6a769faeb2e52004d8be155c7b836f2d
yhupaul <yhupaul@hotmail.com>
2021-07-26 15:12:48 -0700

    refactoring for unit test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,14 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (!(eqArrays(arrayOne, arrayTwo))) {
-    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed`);
+    console.log(`😡😡😡 Assertion Failed`);
   } else {
-    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed`);
+    console.log(`😁😁😁 Assertion Passed`);
   }
 };
 
+module.exports = assertArraysEqual;
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+
 
