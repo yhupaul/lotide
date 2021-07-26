1201e0c43c2ed922abdecf113ecf4f705d233217
yhupaul <yhupaul@hotmail.com>
2021-07-26 14:46:46 -0700

    refactoring for unit testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🙅‍♂🙅‍♂️🙅‍♂️ Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`🙆‍♂️🙆‍♂️🙆‍♂️ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
@@ -20,9 +12,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => "pass"
+module.exports = eqArrays;
