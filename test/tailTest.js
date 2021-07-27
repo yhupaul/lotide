d6595b0ba8a8655acde074d9b5ec4c89b15282b4
yhupaul <yhupaul@hotmail.com>
2021-07-26 17:13:41 -0700

    for mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,11 +2,11 @@ const assert = require('chai').assert;
 const tail   = require('../tail');
 
 describe("#tail", () => {
-  it("returns 3 for [1, 2, 3]", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
 
-  it("returns '5' for ['5']", () => {
+  it("returns null for ['5']", () => {
     assert.deepEqual(tail(['5']), null); 
   });
 });
