c41bf442c3edd71f729616bb4034d26b0e98024c
yhupaul <yhupaul@hotmail.com>
2021-07-27 01:07:59 -0700

    different way to solve

--- flatten.js
+++ flatten.js
@@ -1,6 +1,19 @@
 
+// const flatten = function(array) {
+//   return array.flat();
+// };
 const flatten = function(array) {
-  return array.flat();
+  for (const ele of array) {
+   
+    if(!Array.isArray(ele)) {
+      console.log(ele);
+    }
+      else  {
+        for (const secEle of ele){
+        console.log(secEle);
+      }
+    } 
+  }
 };
-
+//flatten([1, 2, [3, 4], 5, [6]]);
 module.exports = flatten;
