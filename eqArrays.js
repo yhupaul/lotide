220bbecdc7f6e47c2cf28b87ebaa063dd426056c
yhupaul <yhupaul@hotmail.com>
2022-02-08 13:41:12 -0800

    review and add import code

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
@@ -13,3 +14,12 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 module.exports = eqArrays;
+
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
