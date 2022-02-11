37b16098a024dbd6711864aba6cdd1f89e27b2c7
yhupaul <yhupaul@hotmail.com>
2022-02-10 19:10:48 -0800

    review and re organize code

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,4 @@
+const assertArraysEqual = require("./assertArraysEqual");
 
 const takeUntil = function(array, callback) {
   let newArr = [];
@@ -11,14 +12,17 @@ const takeUntil = function(array, callback) {
 };
 
 module.exports = takeUntil;
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
 
-// console.log('---');
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
 
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
+console.log('---');
 
-// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+);
