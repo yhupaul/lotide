9cf7c98dd7d7a25bb0310f247d53be8ed2b61384
yhupaul <yhupaul@hotmail.com>
2022-02-10 19:36:02 -0800

    re organize

--- findKey.js
+++ findKey.js
@@ -1,5 +1,6 @@
+const assertEqual = require('./assertEqual');
 
-const findkey = function(object, callback) {
+const findKey = function(object, callback) {
   for (let key of Object.keys(object)) {
     if (callback(object[key]))  {
       return key;
@@ -7,4 +8,13 @@ const findkey = function(object, callback) {
   }
 };
 
-module.exports = findkey;
+module.exports = findKey;
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
